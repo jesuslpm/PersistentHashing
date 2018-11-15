89fe193ccf42a810c8e4abce9acf9e1fb2a39fc8
Jesús López Méndez <jesus.lopez@i-nercya.com>
2018-11-15 17:48:44 +0100

    Add Copyright

--- PersistentHashing/Memory.cs
+++ PersistentHashing/Memory.cs
@@ -1,15 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Runtime.InteropServices;
-using System.Text;
+﻿/*
+Copyright 2018 Jesús López Méndez
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 
 namespace PersistentHashing
 {
-    /*
-    * Code in this class is taken from RavenDB, https://github.com/ravendb/ravendb
-    * which is licensed as AGPLv3, https://github.com/ravendb/ravendb/blob/v4.1/LICENSE
-    * Permission to use this code is pending.
-    */
+
     public class Memory
     {
 
@@ -36,89 +44,6 @@ namespace PersistentHashing
             {
                 *destination = 0;
             }
-        }
-
-        public static unsafe int Compare(void* p1, void* p2, int size)
-        {
-            byte* bpx = (byte*)p1;
-            byte* bpy = (byte*)p2;
-
-            // PERF: This allows us to do pointer arithmetics and use relative addressing using the 
-            //       hardware instructions without needed an extra register.            
-            long offset = bpy - bpx;
-
-            if ((size & 7) == 0)
-                goto ProcessAligned;
-
-            // We process first the "unaligned" size.
-            ulong xor;
-            if ((size & 4) != 0)
-            {
-                xor = *((uint*)bpx) ^ *((uint*)(bpx + offset));
-                if (xor != 0)
-                    goto Tail;
-
-                bpx += 4;
-            }
-
-            if ((size & 2) != 0)
-            {
-                xor = (ulong)(*((ushort*)bpx) ^ *((ushort*)(bpx + offset)));
-                if (xor != 0)
-                    goto Tail;
-
-                bpx += 2;
-            }
-
-            if ((size & 1) != 0)
-            {
-                int value = *bpx - *(bpx + offset);
-                if (value != 0)
-                    return value;
-
-                bpx += 1;
-            }
-
-        ProcessAligned:
-
-            byte* end = (byte*)p1 + size;
-            byte* loopEnd = end - 16;
-            while (bpx <= loopEnd)
-            {
-                // PERF: JIT will emit: ```{op} {reg}, qword ptr [rdx+rax]```
-                if (*((ulong*)bpx) != *(ulong*)(bpx + offset))
-                    goto XorTail;
-
-                if (*((ulong*)(bpx + 8)) != *(ulong*)(bpx + 8 + offset))
-                {
-                    bpx += 8;
-                    goto XorTail;
-                }
-
-
-                bpx += 16;
-            }
-
-            if (bpx < end)
-                goto XorTail;
-
-            return 0;
-
-        XorTail: xor = *((ulong*)bpx) ^ *(ulong*)(bpx + offset);
-
-        Tail:
-
-            // Fast-path for equals
-            if (xor == 0)
-                return 0;
-
-            // PERF: This is a bit twiddling hack. Given that bitwise xoring 2 values flag the bits difference, 
-            //       we can use that we know we are running on little endian hardware and the very first bit set 
-            //       will correspond to the first byte which is different. 
-
-            bpx += Bits.TrailingZeroesInBytes(xor);
-            return *bpx - *(bpx + offset);
-
         }
     }
 }
